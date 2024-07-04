5ab4d7203e52c3d788304d6f42c3fa4106e246ae
Karan Verma <karanverma@Karans-MacBook-Air.local>
2024-07-05 01:07:37 +0530

    update about us

--- test-app/src/app/about-me/page.jsx
+++ test-app/src/app/about-me/page.jsx
@@ -3,7 +3,10 @@ import PageHeading from "../_components/PageHeading"
 
 const page = () => {
   return (
-    <PageHeading heading="About me"/>
+    <>
+      <PageHeading heading="About me"/>
+      <p>I'm Akash, a passionate frontend developer experienced in creating dynamic and responsive web applications. My expertise includes working with HTML, CSS, JavaScript, and modern frameworks like React and Next JS. I thrive in collaborative environments and am dedicated to continuous learning and keeping up with the latest industry trends.</p>
+    </>
   )
 }
 
